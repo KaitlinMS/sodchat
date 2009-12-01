4a8f25fb7017ecb25e45e7d4069e22d99a460e62
randellma <randellma@b331abf4-de1b-11de-9f73-7335da764322>
2009-12-01 02:54:13 +0000

    Upload 2

---
+++ Matt_SOD/src/Contacts/Contact.java
@@ -0,0 +1,31 @@
+package Contacts;
+
+import java.net.*;
+
+public class Contact {
+
+	private InetAddress ip;
+	private String name;
+
+	public Contact(String i, String n){
+		try{
+			ip = InetAddress.getByName(i);
+			name = n;
+		}
+		catch(UnknownHostException e){}	
+	}
+
+	public String getIp(){
+                return ip.getHostAddress();
+
+	}
+
+	public String getName(){
+                String n = new String(name);
+		return n;
+	}
+
+        public void changeName(String nn){
+            name = nn;
+        }
+}
