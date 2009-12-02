78dea366f63efc28ae8e389a8b7398ce6be8b016
smith.kaitlin <smith.kaitlin@b331abf4-de1b-11de-9f73-7335da764322>
2009-12-02 17:15:46 +0000

    Edits containing formatting changes and commenting

--- trunk/Main/src/Contacts/ContactNetWrapper.java
+++ trunk/Main/src/Contacts/ContactNetWrapper.java
@@ -1,14 +1,19 @@
+/*
+ * ContactNetWrapper.java
+ * Handles network connections related to contacts
+ */
+
 package Contacts;
 
 import java.net.*;
-import java.io.*;
+// This package is unused:
+//import java.io.*;
 
-public class ContactNetWrapper{
+public class ContactNetWrapper {
 
     ContactNetWrapper(Socket s, String cmd, String[] param) {
     }
 
     public void response(Socket s, Boolean b) {
     }
-
 }
