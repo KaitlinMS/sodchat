60427579d1b520534eed39fe9eb65105581e46dd
Randellma <Randellma@b331abf4-de1b-11de-9f73-7335da764322>
2009-12-05 22:37:49 +0000

    Added network method I left out by accident

---
+++ Main/src/Network/NetworkSender.java
@@ -0,0 +1,41 @@
+package Network;
+
+import java.io.*;
+import java.net.*;
+
+public class NetworkSender extends Thread {
+
+    private int port;
+    private String msg;
+    private String ip;
+    private Socket sendSocket;
+
+    public NetworkSender(String message, String sip, int PORT) {
+        msg = message;
+        ip = sip;
+        port = PORT;
+    }
+
+    public Socket Send(String message, String sip) throws Exception {
+        try {
+            sendSocket = new Socket(InetAddress.getByName(ip), port);
+            PrintWriter sout = new PrintWriter(sendSocket.getOutputStream(), true);
+            sout.println(message);
+            return sendSocket;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Socket returnSocket(){
+        super.start();
+        return sendSocket;
+    }
+
+    public void run() {
+        try {
+            Send(msg, ip);
+        } catch (Exception e) {
+        }
+    }
+}
